e54652b4196b4621699652f8639190d26a0d23e4
aabdygaziev <aabdygaziev@gmail.com>
2022-08-18 14:15:16 -0500

    added problem B

---
+++ problemB.js
@@ -0,0 +1,57 @@
+"use strict"
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+      return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// ********** Code Start **********
+function main() {
+  let a = readLine();
+  let inputs = readLine();
+  let queries = readLine();
+
+  inputs = inputs.split(' ').map(num => parseInt(num));
+  queries = queries.split(' ').map(num => parseInt(num));
+  
+  findNum(inputs, queries);
+}
+
+
+function findNum(arr, queries) {
+  
+  for (let i = 0; i < queries.length; i++) {
+
+    let left = -1;
+    let right = arr.length;
+    let num = queries[i];
+    while ((left + 1) < right) {
+      let mid = Math.floor((left + right) / 2);
+
+      if (arr[mid] <= num) {
+        left = mid;
+      } else {
+        right = mid;
+      }
+    }
+    console.log(left + 1);
+  }
+    
+  return 0;
+}
